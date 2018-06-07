ebacc25bb0a5896a16c22ff13be0bdd14f59ecd1
charleswang234 <wang.charles234@gmail.com>
2018-06-07 21:04:01 +0000

    added mapPractice.js and modified filterPractice.js

--- filterPractice.js
+++ filterPractice.js
@@ -1,7 +1,10 @@
 
 var strArray = ["I", "Don't", "like", "eating", "pizza"];
 
-function strLengthLongerThanFive(element) {
+function strLengthLongerThanFive(element, index, array) {
+  // if (index == 1) {
+  //   return false;
+  // }
   if (element.length >= 5) {
     return true;
   }
